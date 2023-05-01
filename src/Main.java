b32b160d9d44836edf6831e5ead7ce8c521c2f82
Stephen <stephen_1996@live.ca>
2023-05-01 14:27:38 -0700

    working at char values

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a Number");
-        int a = scanner.nextInt();
-        System.out.println("value = " + a);
-        // version control test 2
+//        int a = scanner.nextInt();
+//        System.out.println("value = " + a);
+        char c = scanner.next().charAt(0);
+        System.out.println("value = " + c);
     }
 }
