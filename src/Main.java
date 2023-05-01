e21b35b9fcf574503bfc290b44f1a2bb2645a282
Stephen <stephen_1996@live.ca>
2023-05-01 14:00:42 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int a = scanner.nextInt();
+        System.out.println("value = " + a);
+    }
+}
