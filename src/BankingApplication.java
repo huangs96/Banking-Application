35ea06892cf3febcfe48977189eee9e8cfa859dc
Stephen <stephen_1996@live.ca>
2023-05-01 14:50:34 -0700

    initiated transactions and getTransaction methods

--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -14,6 +14,24 @@ class BankAccount
     void deposit(int amount) {
         if (amount != 0) {
             balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount) {
+        if (amount != 0) {
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No Transactions");
         }
     }
 }
